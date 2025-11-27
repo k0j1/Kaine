1626290a0522a69c1e655a380aaa969ef9e21b7f
k0j1 <k0j123t@gmail.com>
2025-11-27 20:11:38 +0900

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 相対パスに設定することで、リポジトリ名に関わらずGitHub Pagesで正しく表示されるようにします
-  base: './', 
+  // GitHub Pagesのリポジトリ名に合わせたベースパスを設定
+  base: '/Kaine/', 
 })
